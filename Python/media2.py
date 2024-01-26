3575a5ff4ebbd4666ff1e19d0fd3e37d1ae72220
Arthur Melo <119857587+ArthMelo@users.noreply.github.com>
2024-01-26 15:11:49 -0300

    Create media2.py

---
+++ Python/media2.py
@@ -0,0 +1,6 @@
+a = float(input("Insira a primeira nota: "))
+b = float(input("Insira a segunda nota: "))
+c = float(input("Insira a terceira nota: "))
+media = ((a*2)+(b*3)+(c*5))/10
+media = round(media, 1)
+print("MEDIA = "+str(media))
